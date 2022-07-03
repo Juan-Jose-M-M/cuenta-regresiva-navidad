d68e4862a805163a578ccf90b64ed3398b1ba25d
Juan José Moreno M <85948893+Juan-Jose-M-M@users.noreply.github.com>
2022-07-03 09:43:28 -0500

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -2,7 +2,7 @@ simplyCountdown('#cuenta', {
 	year: 2022, // required
 	month: 12, // required
 	day: 24, // required
-	hours: 17, // Default is 0 [0-23] integer
+	hours: 23, // Default is 0 [0-23] integer
 	minutes: 59, // Default is 0 [0-59] integer
 	seconds: 59, // Default is 0 [0-59] integer
 	words: { //words displayed into the countdown
